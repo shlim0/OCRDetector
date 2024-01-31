21d8555d6863f0a920dde514c010227296e72419
JJong <jfknfj920@naver.com>
2024-01-31 21:02:30 +0900

    chore: project setting init

---
+++ OCRDetector/OCRDetector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OCRDetector
+//
+//  Created by imseonghyeon on 1/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
