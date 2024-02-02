ba5bdd1be363fd4c3b6daf22d4eeb09ccba08ace
JJong <jfknfj920@naver.com>
2024-02-02 11:41:34 +0900

    feat: Added base Repointing VC, VM & base Preview VC, VM

---
+++ PhotoDetector/PhotoDetector/Presentation/ViewModel/PhotoPreviewViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoPreviewViewModel.swift
+//  PhotoDetector
+//
+//  Created by imseonghyeon on 2/2/24.
+//
+
+import UIKit
+
+final class PhotoPreviewViewModel {
+    
+}
---
+++ PhotoDetector/PhotoDetector/Presentation/ViewModel/PhotoRepointingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoRepointingViewModel.swift
+//  PhotoDetector
+//
+//  Created by imseonghyeon on 2/2/24.
+//
+
+import UIKit
+
+final class PhotoRepointingViewModel {
+    
+}
