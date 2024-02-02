64ab34fcd7d558a827c63612b5a27b66d10fe6c5
JJong <jfknfj920@naver.com>
2024-02-02 11:42:06 +0900

    feat: Added Photo type to save to Repository type

---
+++ PhotoDetector/PhotoDetector/Domain/Entities/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  PhotoDetector
+//
+//  Created by imseonghyeon on 2/2/24.
+//
+
+import UIKit
+
+struct Photo {
+    let id: UUID
+    let date: Date
+    let image: UIImage
+}
