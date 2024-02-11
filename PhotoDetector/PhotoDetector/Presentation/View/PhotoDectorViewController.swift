b431697d7529cbe479cb684deefe1262808f53f3
imSeongHyeon <jfknfj920@naver.com>
2024-02-11 20:46:50 +0900

    feat: Added preview layer and thumnail

--- PhotoDetector/PhotoDetector/Presentation/View/PhotoDectorViewController.swift
+++ PhotoDetector/PhotoDetector/Presentation/View/PhotoDectorViewController.swift
@@ -6,22 +6,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 final class PhotoDetectorViewController: UIViewController {
     // MARK: - Namespace
     private enum Constants {
         static let defaultLayoutMargin: Double = 10.0
     }
-        
+    
     // MARK: - Dependency
-    private var viewModel = PhotoDetectorViewModel(context: CIContext())
+    private var viewModel: PhotoDetectorViewModelProtocol = PhotoDetectorViewModel()
     
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
-        
         bind()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        startDisplay()
+    }
+    
+    func startDisplay() {
+        do {
+            try viewModel.handleDisplay()
+        } catch {
+            print(error)
+        }
     }
     
     // MARK: - View Elements
@@ -38,30 +52,48 @@ final class PhotoDetectorViewController: UIViewController {
         return uiBarButtonItem
     }()
     
-    private let cameraView: UIView = {
+    private lazy var previewLayer: AVCaptureVideoPreviewLayer = {
+        let layer = AVCaptureVideoPreviewLayer()
+        layer.frame = view.bounds
+        layer.videoGravity = .resizeAspectFill
+        
+        return layer
+    }()
+    
+    private lazy var wrappedPreviewLayerView: UIView = {
         let view = UIView()
-        view.backgroundColor = .black
+        view.layer.addSublayer(previewLayer)
         view.translatesAutoresizingMaskIntoConstraints = false
         
         return view
     }()
     
-    private lazy var thumbnail: UIButton = {
+    private let thumbnail: UIButton = {
         let button = UIButton()
         button.backgroundColor = .black
         button.contentMode = .scaleAspectFit
-        button.translatesAutoresizingMaskIntoConstraints = false
         button.addTarget(self, action: #selector(thumbnailHandler), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
         
         return button
     }()
     
-    private let shutterButton: UIButton = {
+    private lazy var shutterButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(systemName: "circle.dashed.inset.filled"), for: .normal)
+        button.addTarget(self, action: #selector(shutterButtonHandler), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         
         return button
+    }()
+    
+    private lazy var bottomView: UIView = {
+        let view = UIView()
+        view.addsubViews(thumbnail, shutterButton)
+        view.backgroundColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
     }()
 }
 
@@ -72,17 +104,22 @@ extension PhotoDetectorViewController {
         let photoPreviewViewController = PhotoPreviewViewController()
         navigationController?.pushViewController(photoPreviewViewController, animated: true)
     }
+    
+    @objc
+    private func shutterButtonHandler() {
+        viewModel.didTapShutterButton()
+    }
 }
 
 // MARK: - Configuration
 extension PhotoDetectorViewController {
     private func configureView() {
-        view.backgroundColor = .white
-        view.addsubViews(cameraView, thumbnail, shutterButton)
+        view.addsubViews(wrappedPreviewLayerView, bottomView)
+        
         configureNavigationBar()
         configureConstraint()
     }
-
+    
     private func configureNavigationBar() {
         navigationController?.navigationBar.backgroundColor = .defaultNavigationBarColor
         navigationItem.setLeftBarButton(navigationLeftButton, animated: true)
@@ -90,35 +127,65 @@ extension PhotoDetectorViewController {
     }
     
     private func configureConstraint() {
-        let safeArea = view.safeAreaLayoutGuide
+        configureConstraintWrappedPreviewLayerView()
+        configureConstraintBottomView()
+        configureConstraintThumbnail()
+        configureConstraintShutterButton()
+    }
+    
+    private func configureConstraintWrappedPreviewLayerView() {
         NSLayoutConstraint.activate([
-            cameraView.topAnchor.constraint(equalTo: safeArea.topAnchor),
-            cameraView.bottomAnchor.constraint(equalTo: thumbnail.topAnchor, constant: -Constants.defaultLayoutMargin),
-            cameraView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
-            cameraView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+            wrappedPreviewLayerView.topAnchor.constraint(equalTo: view.topAnchor),
+            wrappedPreviewLayerView.bottomAnchor.constraint(equalTo: bottomView.topAnchor),
+            wrappedPreviewLayerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            wrappedPreviewLayerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            wrappedPreviewLayerView.heightAnchor.constraint(equalTo: bottomView.heightAnchor, multiplier: 8.0)
         ])
-        
+    }
+    
+    private func configureConstraintBottomView() {
         NSLayoutConstraint.activate([
-            thumbnail.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: Constants.defaultLayoutMargin),
-            thumbnail.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+            bottomView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            bottomView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            bottomView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
-        
+    }
+    
+    private func configureConstraintThumbnail() {
         NSLayoutConstraint.activate([
-            shutterButton.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
-            shutterButton.centerYAnchor.constraint(equalTo: thumbnail.centerYAnchor)
+            thumbnail.topAnchor.constraint(equalTo: bottomView.topAnchor, constant: Constants.defaultLayoutMargin),
+            thumbnail.bottomAnchor.constraint(equalTo: bottomView.bottomAnchor, constant: -Constants.defaultLayoutMargin * 3),
+            thumbnail.leadingAnchor.constraint(equalTo: bottomView.leadingAnchor, constant: Constants.defaultLayoutMargin),
+            thumbnail.widthAnchor.constraint(equalTo: thumbnail.heightAnchor),
+        ])
+    }
+    
+    private func configureConstraintShutterButton() {
+        NSLayoutConstraint.activate([
+            shutterButton.topAnchor.constraint(equalTo: bottomView.topAnchor, constant: Constants.defaultLayoutMargin),
+            shutterButton.bottomAnchor.constraint(equalTo: thumbnail.bottomAnchor),
+            shutterButton.centerXAnchor.constraint(equalTo: bottomView.centerXAnchor)
         ])
     }
     
     private func bind() {
-        viewModel.rectangleListener = updateRectangleView
+        viewModel.photoListener = updateRectangleView
         viewModel.thumbnailListener = updateThumbnailView
     }
     
-    private func updateRectangleView() {
+    private func updateRectangleView(photo: PhotoOutput) {
+        previewLayer.session = photo.sesson
         
+        Task { @MainActor in
+            previewLayer.sublayers?.removeSubrange(1...)
+            
+            if let rectangle = photo.rectangle {
+                previewLayer.addSublayer(rectangle.layer)
+            }
+        }
     }
     
-    private func updateThumbnailView() {
-        
+    private func updateThumbnailView(thumbnail: UIImage) {
+        self.thumbnail.setImage(thumbnail, for: .normal)
     }
 }
